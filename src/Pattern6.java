2573e8fc304817b13db082faab94142a1fdd7bdc
Rimjhimm12 <rimjhimmallick6@gmail.com>
2025-09-15 12:24:02 -0500

    Create Pattern6.java

---
+++ src/Pattern6.java
@@ -0,0 +1,15 @@
+public class Pattern6 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            for (int k = 1; k < i; k++) {
+                System.out.print(" ");
+            }
+            for (int j = 10; j > i; j--) {
+                System.out.print("* ");
+            }
+
+            System.out.println();
+        }
+    }
+
+}
