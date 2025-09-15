f6e657b09b141ab70c85ca81b0306f475d275237
Rimjhimm12 <rimjhimmallick6@gmail.com>
2025-09-15 12:23:59 -0500

    New patterns

--- src/Pattern2.java
+++ src/Pattern2.java
@@ -1,8 +1,8 @@
 public class Pattern2 {
     public static void main(String[] args) {
-        for(int i = 0; i<=3; i++){
+        for(int i = 0; i<=10; i++){
 
-            for(int j = 3; j>=i; j--){
+            for(int j = 10; j>=i; j--){
                 System.out.print("*");
             }
             System.out.println("");
---
+++ src/Pattern4.java
@@ -0,0 +1,17 @@
+public class Pattern4 {
+    public static void main(String[] args) {
+        for(int i = 1; i<=10; i++){
+            for(int k =1; k<i; k++){
+                System.out.print(" ");
+            }
+            for(int j = 10; j>=i; j--){
+                System.out.print("*");
+                }
+            System.out.println("");
+            }
+
+
+
+        }
+    }
+
